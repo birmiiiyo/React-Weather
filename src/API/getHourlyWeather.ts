365138373af7788ec002b57bbf6443bd0fdf3e75
BirmiiiYo <avramenkoa773@gmail.com>
2023-01-13 19:28:51 +0300

    add hourly time weather

---
+++ src/API/getHourlyWeather.ts
@@ -0,0 +1,23 @@
+import { StormGlass } from '@types/StormGlass'
+import { IPositionProps } from '@types/LatLonProps';
+import axios from 'axios'
+const time = new Date()
+time.toUTCString
+export const getHourlyWeatherAPI = async ({lat,lon}: IPositionProps)=>{
+     const request = await axios<StormGlass>({
+        url: `${process.env.STORMGLASS_PATH}`,
+        method: 'get',
+        headers:{
+            'Authorization': `${process.env.STORM_GLASS_API_key}`
+        },
+        params:{
+            lat,
+            lng: lon,
+            params: 'airTemperature,pressure,currentSpeed,cloudCover',
+            start: new Date().getTime(),
+            end: new Date().getTime()+5*60*60*1000
+        }
+    })
+    console.log(request);
+    return request.data
+}
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -10,6 +10,7 @@ import { useAppDispatch } from 'hooks/useAppDispatch';
 import { addLocation } from '@store/actions/LocationActions';
 import { getDailyWeather } from '@store/actions/DailyWeatherActions';
 import { getCurrentTime } from '@store/actions/TimeActions';
+import { getHourlyWeatherAPI } from 'API/getHourlyWeather';
 
 const bgImg = 'https://krot.info/uploads/posts/2021-12/thumbs/1638363519_2-krot-info-p-solnechnii-peizazh-krasivie-foto-2.jpg'
 const Img = "https://funart.pro/uploads/posts/2021-03/1617081925_43-p-oboi-solnechnii-peizazh-43.jpg"
@@ -33,11 +34,11 @@ function App() {
       }
 })
   },[])
-  
   return (
     <Background bodyImage={bgImg}>
       <Container image={Img}>
         <Form/>
+        <button onClick={()=> getHourlyWeatherAPI({lat:27,lon:53})}>hourly weather</button>
         <Clock/>
         <Information/>
         <Weather/>
