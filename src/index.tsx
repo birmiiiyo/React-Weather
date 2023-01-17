aca41184d3c7a260b662802ee45dbb426ef65cbd
BirmiiiYo <avramenkoa773@gmail.com>
2023-01-17 15:54:07 +0300

    add redux-persist and create loader

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import GlobalStyle from './styles/global'
 import { Provider } from 'react-redux';
-import {store} from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './store';
+import { Loader } from '@components/Loader';
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 root.render( 
  <Provider store={store}>
+  <PersistGate loading={<Loader/>} persistor={persistor}>	 
   <GlobalStyle/>
   <App />
+  </PersistGate>
   </Provider>
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,26 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer,persistStore } from 'redux-persist';
 import createSagaMiddleware from '@redux-saga/core';
-
+import storage from 'redux-persist/lib/storage'
 import { rootSaga } from '@saga/index'
 
 import { rootReducer } from './reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['calendar','location']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const sagaMiddleWare = createSagaMiddleware();
 
 export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleWare)),
-);
+  persistedReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleWare)),
+  )
+
+export const persistor = persistStore(store)
 
 sagaMiddleWare.run(rootSaga)
 
